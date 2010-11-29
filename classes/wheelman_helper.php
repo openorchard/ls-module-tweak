91cad98e7a6d59756ac68846d93f3e6e5af9a3e5
Eric Muyser <eric.muyser@gmail.com>
2010-11-28 20:12:19 -0800

    Updated.

--- classes/wheelman_helper.php
+++ classes/wheelman_helper.php
@@ -12,32 +12,6 @@ class Wheelman_Helper {
   }
   
   public function on_handleRequest() {
-    $controller = Cms_Controller::create();
-
-    $cms_update_elements = post('cms_update_elements');
-
-    if(!$cms_update_elements) // let's get out of here!
-      return;
-
-    foreach($cms_update_elements as $element => $partial)
-    {
-      if($partial == 'ls_cms_page') {
-        unset($_POST['cms_update_elements'][$element]);
-        
-        $params = array();
-        
-        $page = Cms_Page::findByUrl(Phpr::$request->getCurrentUri(), $params);
-        
-        ob_start();
-        $controller->open($page, $params);
-        ob_end_clean();
-      
-        echo ">>" . $element . "<<";
-        
-        $controller->render_page();
-      }
-      
-      return; // we're done here
-    }
+    
   }
 }
--- classes/wheelman_module.php
+++ classes/wheelman_module.php
@@ -1,26 +1,72 @@
 <?
 
 class Wheelman_Module extends Core_ModuleBase {
-
+  public $cms_update_element;
+  
   protected function createModuleInfo()
   {
-    $helper = new Wheelman_Helper();
-    $helper->on_handleRequest();
-    
-    return new Core_ModuleInfo(
+    $info = new Core_ModuleInfo(
       "Wheelman",
       "Limewheel skinning helper",
       "Limewheel Creative Inc."
     );
+     
+    $helper = new Wheelman_Helper();
+    $helper->on_handleRequest();
+
+    $cms_update_elements = post('cms_update_elements');
+
+    if(!$cms_update_elements) // let's get out of here!
+      return $info;
+      
+    foreach($cms_update_elements as $element => $partial)
+    {
+			if(!$element) {
+				unset($_POST['cms_update_elements'][$element]);
+				
+				continue;
+			}
+			
+      if($partial == 'ls_cms_page') {
+        unset($_POST['cms_update_elements'][$element]);
+        
+        $this->cms_update_element = $element;
+      }
+    }
+  
+    return $info;
   }
   
   public function subscribeEvents()
   {
+    Backend::$events->addEvent('cms:onAfterHandleAjax', $this, 'before_handle_ajax');
     Backend::$events->addEvent('cms:onBeforeDisplay', $this, 'before_page_display');
   }
+  
+  public function before_page_display() {
+    $controller = Cms_Controller::get_instance();
+    $controller->data['site_settings'] = $controller->render_partial('site:settings');
+  }
 
-  public function before_page_display($page)
+  public function before_handle_ajax()
   {
-
+    if(!$this->cms_update_element)
+      return;
+    
+    $params = array();
+    
+    $controller = Cms_Controller::get_instance();
+    $controller->data['site_settings'] = $controller->render_partial('site:settings');
+    $controller->action();
+    
+    $page = Cms_Page::findByUrl(Phpr::$request->getCurrentUri(), $params);
+    
+    ob_start();
+    $controller->open($page, $params);
+    ob_end_clean();
+  
+    echo ">>" . $this->cms_update_element . "<<";
+    
+    $controller->render_page();
   }
 }
