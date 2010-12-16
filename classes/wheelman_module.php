78a9acdd3aace7f5a9312d5ff5f7356bc39841dc
Eric Muyser <eric.muyser@gmail.com>
2010-12-16 15:08:26 -0800

    Fix minor bug. Add helper function.

--- classes/wheelman_module.php
+++ classes/wheelman_module.php
@@ -34,22 +34,27 @@ class Wheelman_Module extends Core_ModuleBase {
   
   public function subscribeEvents() {
     Backend::$events->addEvent('cms:onBeforeHandleAjax', $this, 'before_handle_ajax');
+    Backend::$events->addEvent('cms:onAfterHandleAjax', $this, 'after_handle_ajax');
     Backend::$events->addEvent('cms:onBeforeDisplay', $this, 'before_page_display');
   }
   
   public function before_page_display() {
+		// we want $site_settings for non-AJAX
     $controller = Cms_Controller::get_instance();
     $controller->data['site_settings'] = $controller->render_partial('site:settings');
   }
 
   public function before_handle_ajax() {
-		// we want $site_settings either way
+		// we want $site_settings for AJAX
     $controller = Cms_Controller::get_instance();
     $controller->data['site_settings'] = $controller->render_partial('site:settings');
-		
+  }
+	
+	public function after_handle_ajax() {
     if(!$this->cms_update_element)
       return;
     
+		$controller = Cms_Controller::get_instance();
     $controller->action();
     
     $params = array(); // does nothing
@@ -62,5 +67,5 @@ class Wheelman_Module extends Core_ModuleBase {
     echo ">>" . $this->cms_update_element . "<<";
     
     $controller->render_page();
-  }
+	}
 }
